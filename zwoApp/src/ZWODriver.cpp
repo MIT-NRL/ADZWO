f184ca3d01d2b6bf81ac4a2400669a1ec4757c87
Sean Fayfar <sfayfar@gmail.com>
2024-07-25 14:52:14 -0400

    Bump version to 0.1.1

--- zwoApp/src/ZWODriver.cpp
+++ zwoApp/src/ZWODriver.cpp
@@ -20,7 +20,7 @@
 #include <iocsh.h>
 
 static const char *driverName = "ZWODriver";
-static const char *driverVersion = "0.1.0";
+static const char *driverVersion = "0.1.1";
 
 static void ZWODriverCaptureTaskC(void *drvPvt) {
     ZWODriver *driver = (ZWODriver *)drvPvt;
