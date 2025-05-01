b3b0ed742763b1ef4138ce1d5b301c6e3a85668e
Sean Fayfar <93339634+sfayfar@users.noreply.github.com>
2025-05-01 13:46:02 -0400

    Version 0.3.0

--- zwoApp/src/ZWODriver.cpp
+++ zwoApp/src/ZWODriver.cpp
@@ -20,7 +20,7 @@
 #include <iocsh.h>
 
 static const char *driverName = "ZWODriver";
-static const char *driverVersion = "0.2.0";
+static const char *driverVersion = "0.3.0";
 
 static void ZWODriverCaptureTaskC(void *drvPvt) {
     ZWODriver *driver = (ZWODriver *)drvPvt;
